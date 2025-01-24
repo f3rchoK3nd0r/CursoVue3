6eab14fba8f88323a679fc4da5422c3deb33301a
f3rchoK3nd0r <mistramitesinai@gmail.com>
2025-01-23 23:56:38 -0700

    Creamos componente Contador y lo llamamos desde App.vue

---
+++ vue-project/src/components/Contador.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <h1>Hola Lou!</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+  const contador = ref(0);
+  const incrementar = () =>{
+    contador.value++;
+  }
+  const decrementar = () =>{
+    contador.value--;
+  }
+
+</script>
+
+<style scoped>
+p {
+  color: greenyellow;
+  width: 35px;
+}
+</style>
